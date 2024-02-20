b0c04ae5981edb647895978182e64cd3ae17d4b8
augustofklein <augusto_klein@hotmail.com>
2024-02-20 11:37:07 -0300

    Recriacao do projeto com o standalone desabilitado

--- FrontEnd/projeto-angular/src/app/app.component.spec.ts
+++ FrontEnd/projeto-angular/src/app/app.component.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -13,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'projeto-angular' title`, () => {
+  it(`should have as title 'projeto-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('projeto-angular');
